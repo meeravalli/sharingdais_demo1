a971bcc9e31569ecdfb38b469dbcf8c1feb013f6
roxshahid <rox.shahid@gmail.com>
2014-09-19 19:49:32 +0530

    edit in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,21 +25,34 @@ Sharingdasiss::Application.configure do
   
   
   #mailer configuration
-  config.action_mailer.default_url_options = { host: 'sharingdais.com' }
+  config.action_mailer.default_url_options = { :host => 'sharingdais.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "sharingdais.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'admin@sharingdais.com',
+    password: 'Sharing654321@#'
+  }  
 
 # SMTP Method for GMAIL #
-config.action_mailer.default :charset => "utf-8"
- config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'baci.lindsaar.net',
-  :user_name            => 'admin@sharingdais.com',
-  :password             => 'Sharing654321@#',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
- config.action_mailer.perform_deliveries = true
- config.action_mailer.raise_delivery_errors = true
+#config.action_mailer.default :charset => "utf-8"
+#config.action_mailer.delivery_method = :smtp
+#config.action_mailer.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :domain               => 'baci.lindsaar.net',
+#  :user_name            => 'admin@sharingdais.com',
+#  :password             => 'Sharing654321@#',
+#  :authentication       => 'plain',
+#  :enable_starttls_auto => true  }
+# config.action_mailer.perform_deliveries = true
+# config.action_mailer.raise_delivery_errors = true
 
 # SMTP Method for GMAIL #
 
