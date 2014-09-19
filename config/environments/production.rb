ba791235d916c11647880b6a3840a5b729a038b9
roxshahid <rox.shahid@gmail.com>
2014-09-19 20:14:54 +0530

    edit in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Sharingdasiss::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "mail.sharingdais.com",
     port: 587,
     domain: "sharingdais.com",
     authentication: "plain",
