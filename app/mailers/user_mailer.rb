aec8d5781d6b7b3c35ea075e2ccca7e6a461d9e1
roxshahid <rox.shahid@gmail.com>
2014-09-24 11:25:13 +0530

    edit in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -92,6 +92,6 @@ class UserMailer < ActionMailer::Base
     @email = @data[:email]
     @phon = @data[:phon]
     @message = @data[:message]
-    mail(to: "support@sharingdais.com", :subject => 'Feed back')
+    mail(to: "support@sharingdais.com", :subject => 'Feedback Mail')
   end
 end
