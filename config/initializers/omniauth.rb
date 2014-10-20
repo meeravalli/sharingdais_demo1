0655dc4a66449f8a364a038988c6a355dd0b4bf6
roxshahid <rox.shahid@gmail.com>
2014-10-20 11:43:23 +0530

    edit in initial box

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	if Rails.env.development?
-  		provider :facebook, '1472848959663464', '4c84446c2e947eae9dd349ef630b474e', {:provider_ignores_state => true}
+  		provider :facebook, '297568747083890', '62cf2e52043a1e460338407c67a0a50d', {:provider_ignores_state => true}
   		provider :google_oauth2, '573011007380-ccl5uk12ivu2h8s102r2hf4b3tkah08j.apps.googleusercontent.com', 'iILALYPrIYsBtEnRkOLLwCet'
     else
     	provider :facebook, '1472848959663464', '4c84446c2e947eae9dd349ef630b474e', {:provider_ignores_state => true}
