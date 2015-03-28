390e7ee30eb62063a585983218565e1988277f37
unknown <rox.shahid@gmail.com>
2015-03-28 12:24:34 +0530

    search error solved

--- app/controllers/peer_services_controller.rb
+++ app/controllers/peer_services_controller.rb
@@ -1,13 +1,15 @@
 class PeerServicesController < ApplicationController
 layout 'peer_service'
   def index
-    #puts "======================#{params[:search][:skill_type_id]}================="
+    
    @search_params = nil
    if params[:search]
-     session[:city_id] = params[:search][:city_id]
+     ci = Location.where(:id => params[:search][:location_id]).first
+     session[:city_id] = ci.city.id
      location = params[:search][:location_id]
      key = params[:search][:peer_category_id]
-     city = params[:search][:city_id]
+     city = ci.city.id
+     params[:search][:city_id] = ci.city.id
      query = ""
       if params[:search][:peer] == "0"
        query += "seeker_provider = 0"
@@ -19,12 +21,7 @@ layout 'peer_service'
             query += " and peer_category_id = #{key}"
         end
       end
-      #if city.present?
-      #   query += " and city_id = #{city}"
-      #end
-      #if location.present?
-      #   query += " and location_id = #{location}"
-      #end
+     
       if params[:search].has_key?("include_near_by_locations")
         location = Location.where("id = ?",params[:search][:location_id]).last
         city = City.where("id = ?",params[:search][:city_id]).last
@@ -38,7 +35,7 @@ layout 'peer_service'
       @locations = params[:search][:city_id].blank? ? [] : City.find(params[:search][:city_id]).locations
       @page = params[:page] || 1
    end
-     redirect_to "/#{@city.city_name}/#{@location.location_name}/peer_services?key=#{params[:search][:peer_category_id]}&peer=#{params[:search][:peer]}" unless @city.nil?
+     #redirect_to "/#{@city.city_name}/#{@location.location_name}/peer_services?key=#{params[:search][:peer_category_id]}&peer=#{params[:search][:peer]}" unless @city.nil?
   end
 
   def peer_result
@@ -51,5 +48,5 @@ layout 'peer_service'
     puts "====#{@search_results}==================================================="
   end
 
-	
+  
 end
