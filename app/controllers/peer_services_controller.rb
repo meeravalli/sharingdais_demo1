a24ee492f579eebc171cc618fef4a884d242c72d
unknown <rox.shahid@gmail.com>
2015-04-04 16:34:55 +0530

    peer service url added

--- app/controllers/peer_services_controller.rb
+++ app/controllers/peer_services_controller.rb
@@ -1,3 +1,4 @@
+
 class PeerServicesController < ApplicationController
 layout 'peer_service'
   def index
@@ -9,6 +10,7 @@ layout 'peer_service'
      location = params[:search][:location_id]
      key = params[:search][:peer_category_id]
      city = ci.city.id
+     @city = City.find(ci.city.id)
      params[:search][:city_id] = ci.city.id
      query = ""
       if params[:search][:peer] == "0"
@@ -34,8 +36,43 @@ layout 'peer_service'
       @search_params = @search_results.count
       @locations = params[:search][:city_id].blank? ? [] : City.find(params[:search][:city_id]).locations
       @page = params[:page] || 1
-   end
-     #redirect_to "/#{@city.city_name}/#{@location.location_name}/peer_services?key=#{params[:search][:peer_category_id]}&peer=#{params[:search][:peer]}" unless @city.nil?
+
+    elsif  params[:city].present?
+      ci = City.where(:city_name => params[:city]).first
+      loc = Location.where(:location_name => params[:location]).first
+     session[:city_id] = ci.id
+     location = loc.id
+     key = params[:key]
+     city = ci.id
+     # params[:search][:city_id] = ci.id
+     # params[:search][:location_id] = loc.id
+     query = ""
+      if params[:peer] == "0"
+       query += "seeker_provider = 0"
+      else
+       query += "seeker_provider = 1"
+      end
+      if key != '1'
+        if key.present?
+            query += " and peer_category_id = #{key}"
+        end
+      end
+     
+      #if params[:search].has_key?("include_near_by_locations")
+        location = Location.where("id = ?",loc.id).last
+        city = City.where("id = ?",city).last
+        search_area = [city.city_name, location.location_name, "India"].join(", ")
+        @search_results = PeerServicePostRequirement.near("#{search_area}", 10).where(query).paginate(:page => params[:page], :per_page => 15)    
+      #else
+        #@search_results = PeerServicePostRequirement.where(query).paginate(:page => params[:page], :per_page => 15)
+      #end 
+      #@search_results = SkillPostRequirement.where(query).paginate(:page => params[:page], :per_page => 25)
+      @search_params = @search_results.count
+      @locations = params[:city].blank? ? [] : ci.locations
+      @page = params[:page] || 1
+  
+      end
+    redirect_to "/#{city.city_name}/#{location.location_name}/peer_services?key=#{params[:search][:peer_category_id]}&peer=#{params[:search][:peer]}" unless @city.nil?
   end
 
   def peer_result
@@ -50,3 +87,4 @@ layout 'peer_service'
 
   
 end
+
